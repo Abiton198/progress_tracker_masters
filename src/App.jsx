d454381c4fe820d26a3e585835dc2d0a41915e16
abiton198 <abitonp@gmail.com>
2024-12-21 12:09:14 +0200

    Tailwind CSS setup

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <>
-  
+      <h1 className='text-white text-3xl mt-10'>Masters NLP</h1>
       <p className="read-the-docs">
         Click here to check progress...
       </p>
