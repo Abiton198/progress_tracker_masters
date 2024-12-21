ea3ef22637ccac77586b5cfd2f306d826406ba4c
abiton198 <abitonp@gmail.com>
2024-12-21 11:55:37 +0200

    Project set up with vite

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+  
+      <p className="read-the-docs">
+        Click here to check progress...
+      </p>
+    </>
+  )
+}
+
+export default App
