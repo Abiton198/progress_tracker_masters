39613b1abf72bfedb20a1fd4addaf594b76685ed
abiton198 <abitonp@gmail.com>
2024-12-22 10:02:46 +0200

    addTaskInput function updated - renders a field and button for adding new tasks

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,3 @@
 export {default as calculateProgress} from './calculateProgress'
-export {default as AddTask} from './addTask'
+export {default as addTask} from './addTask'
 export {default as toggleTaskCompletion} from './toggleTaskCompletion'
